d6701aa6266f2a606aa84824213fbd74f1359ceb
Pranav Bhawsar <137757814+Pranavbh1@users.noreply.github.com>
2023-10-17 14:21:20 +0530

    Update D08P1_LeetCode_268.py

--- Python Solutions/D08P1_LeetCode_268.py
+++ Python Solutions/D08P1_LeetCode_268.py
@@ -1,6 +1,6 @@
 #Given an array nums containing n distinct numbers in the range [0, n], 
 #return the only number in the range that is missing from the array.
-
+#way_1
 class Solution:
   def missingNumber(self, nums: List[int]) -> int:
       real_sum = 0
@@ -11,3 +11,12 @@ class Solution:
           missing_sum += num
       
       return real_sum - missing_sum
+#Way_2
+#Robust way 
+#Sum of range [0,n] - sum of given list                #substraction
+num_list = list(map(int, input().split(",")))
+n = len(num_list)
+sum_n = []
+for i in num_list:
+    sum_n.append(i)
+print((n*(n+1)//2)-sum(sum_n))
