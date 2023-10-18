649f61d3be7edf03cd3edaafc7f498c93c9ede52
Adepu Srujana <90594626+adepusrujana@users.noreply.github.com>
2023-10-18 10:17:37 +0530

    Update D01P1_LeetCode_1480.py

--- Python Solutions/D01P1_LeetCode_1480.py
+++ Python Solutions/D01P1_LeetCode_1480.py
@@ -1,9 +1,5 @@
-from typing import List
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        sum = 0
-        run_sum = []
-        for i in range(len(nums)):
-            sum += nums[i]
-            run_sum.append(sum)
-        return run_sum
+        for i in range(1,len(nums)):
+            nums[i]=nums[i]+nums[i-1]
+        return nums
