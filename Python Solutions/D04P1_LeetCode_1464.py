1bc1fd3dcd0b2c64bb7568e054e51d53026d7f3a
pavithragithu <125376819+pavithragithu@users.noreply.github.com>
2023-10-19 10:28:48 -0700

    Update D04P1_LeetCode_1464.py

--- Python Solutions/D04P1_LeetCode_1464.py
+++ Python Solutions/D04P1_LeetCode_1464.py
@@ -1,4 +1,10 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        nums.sort(reverse=True)
-        return (nums[0]-1) * (nums[1]-1) 
+        v1=v2=0
+        for num in nums:
+            if num>v1:
+                v2=v1
+                v1=num
+            elif num>=v2:
+                v2=num
+        return ((v1-1)*(v2-1))
