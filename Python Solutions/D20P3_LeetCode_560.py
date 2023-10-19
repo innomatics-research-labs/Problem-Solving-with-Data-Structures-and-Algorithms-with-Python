326f57c079121410a27d8ae4a78bca03aec5e64a
Pranav Bhawsar <137757814+Pranavbh1@users.noreply.github.com>
2023-10-19 10:32:57 +0530

    Update D20P3_LeetCode_560.py
    In this code we are taking global and local variable and we are running nested for loops , the we are update our local variable accordingly and similarly we are updating our global variable .

--- Python Solutions/D20P3_LeetCode_560.py
+++ Python Solutions/D20P3_LeetCode_560.py
@@ -1,1 +1,12 @@
-# Python3 code coming soon
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        result = 0
+        # N = len(nums)
+        for i in range(len(nums)):
+            temp = 0
+            for j in range(i, len(nums)):
+                temp += nums[j]
+                if temp == k:
+                    result += 1
+        return result
+
