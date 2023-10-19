3872c5fd45822902b3f2b0f4aa9cca95b3d1c688
Pravalika <114800594+EPravalika96@users.noreply.github.com>
2023-10-19 14:03:13 +0530

    Update D03P1_LeetCode_1365.py
    How many numbers are smaller than current number in an array

--- Python Solutions/D03P1_LeetCode_1365.py
+++ Python Solutions/D03P1_LeetCode_1365.py
@@ -3,16 +3,12 @@
 
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        
-        l = [0]*len(nums)
-        
-        for i in range(len(nums)):
-            count=0
-            for j in range(len(nums)):
-                if nums[i]>nums[j]:
+        res = []
+        for i in range(0,len(nums)):
+            count= 0
+            for j in range(0,len(nums)):
+                if j!=i and nums[j]<nums[i]:
                     count+=1
-                    
-            l[i] = count
+            res.append(count)
+        return res
             
-            
-        return l
