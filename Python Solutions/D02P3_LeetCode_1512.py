bc7f74450f0635813af476fe2761aec4348d4bfa
shailajaburla <55321930+BURLASHAILAJA@users.noreply.github.com>
2023-10-25 20:56:00 +0530

    Update D02P3_LeetCode_1512.py
    1. I used two for loops.
    2. Next, using if control statement. I wrote condition inside if condition.
    3. Finally I got desire output.

--- Python Solutions/D02P3_LeetCode_1512.py
+++ Python Solutions/D02P3_LeetCode_1512.py
@@ -1,14 +1,9 @@
-# Python3 , By:- Harsh Udai
-# Que. Number of Good Pairs
-
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        count=0
-        for i in range(0,len(nums)):
+        c=0
+        for i in range(len(nums)):
             for j in range(i+1,len(nums)):
-                if(i<j and nums[i]==nums[j]):
-                    count+=1
-                    
-                    
-        return count
+                if (nums[i]==nums[j] and i<j):
+                    c+=1 
+        return c 
         
