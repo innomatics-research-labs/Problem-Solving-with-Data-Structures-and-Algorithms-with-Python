547ed7bf0cd083dc848c303cb59f271d5be21476
Krystizz <148135964+Krystizz@users.noreply.github.com>
2023-10-26 15:39:55 +0530

    Update D28P1_LeetCode_144.py

--- Python Solutions/D28P1_LeetCode_144.py
+++ Python Solutions/D28P1_LeetCode_144.py
@@ -1,1 +1,13 @@
-# Python3 code coming soon
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+
+        def preorder(node):
+            if node is None:
+                return
+            result.append(node.val)
+            preorder(node.left)
+            preorder(node.right)
+
+        preorder(root)
+        return result
