f16ae8406b9645cf78d0908f843c57738bc0b9ff
Pranav Bhawsar <137757814+Pranavbh1@users.noreply.github.com>
2023-10-27 13:16:05 +0530

    Update D26P1_LeetCode_160.py
    In this code we are a condition with statement id this becomes TRUE then it enters and check another condition if this also becomes TRUE then we are updating h1 else we we are moving to the next item in our iterration i.e. .next().

--- Python Solutions/D26P1_LeetCode_160.py
+++ Python Solutions/D26P1_LeetCode_160.py
@@ -1,1 +1,17 @@
-# Python3 code coming soon
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        h1 = headA
+        h2 = headB
+        
+        while h1 != h2:
+            if not h1:
+                h1 = headB
+            else:
+                h1 = h1.next
+            
+            if not h2:
+                h2 = headA
+            else:
+                h2 = h2.next
+                
+        return h1
