6d07c243464717bf6443cd8aff6203b3886c9671
amreenshaikh777 <141799084+amreenshaikh777@users.noreply.github.com>
2023-10-27 15:02:49 +0530

    Update D16P2_LeetCode_1079.py

--- Python Solutions/D16P2_LeetCode_1079.py
+++ Python Solutions/D16P2_LeetCode_1079.py
@@ -1,1 +1,14 @@
-# Python3 code coming soon
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        def backtrack(counter):
+            count = 0
+            for tile in counter:
+                if counter[tile] > 0:
+                    counter[tile] -= 1
+                    count += 1 + backtrack(counter)
+                    counter[tile] += 1
+            return count
+
+        from collections import Counter
+        counter = Counter(tiles)
+        return backtrack(counter)
