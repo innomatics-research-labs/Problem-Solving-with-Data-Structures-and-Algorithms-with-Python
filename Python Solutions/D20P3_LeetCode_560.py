7ecd183d909aa61eb1d33905b7824efeda36648e
Pranav Bhawsar <137757814+Pranavbh1@users.noreply.github.com>
2023-10-27 15:23:17 +0530

    Update D20P3_LeetCode_560.py
    Counter is a Python library that counts the frequency of elements in a sequence. It creates a dictionary-like object that calculates the occurrences of every element in a list, tuple, or string.

--- Python Solutions/D20P3_LeetCode_560.py
+++ Python Solutions/D20P3_LeetCode_560.py
@@ -1,1 +1,10 @@
-# Python3 code coming soon
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = collections.Counter()
+        count[0] = 1
+        ans = sum = 0
+        for x in nums:
+            sum += x
+            ans += count[sum-k]
+            count[sum] += 1
+        return ans  
