69f04610ba78278c379d3e10bb0eb94110ced8b2
pavithra <125376819+pavithragithu@users.noreply.github.com>
2023-10-30 04:24:59 -0700

    Update D11P1_LeetCode_162.py

--- Python Solutions/D11P1_LeetCode_162.py
+++ Python Solutions/D11P1_LeetCode_162.py
@@ -1,4 +1,7 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-         return nums.index(max(nums))
+        max_num=max(nums)
+        for i in range(len(nums)):
+            if nums[i]==max_num:
+                return i
         
