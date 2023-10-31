5c391f6f6aac126e50b828f017a8c5c69073377f
pavithra <125376819+pavithragithu@users.noreply.github.com>
2023-10-31 02:17:31 -0700

    Update D12P1_LeetCode_977.py

--- Python Solutions/D12P1_LeetCode_977.py
+++ Python Solutions/D12P1_LeetCode_977.py
@@ -1,6 +1,8 @@
-# Python3 code coming soon
+
 class Solution:
-    def sortedSquares(self, A: List[int]) -> List[int]:
-        return sorted([x*x for x in A])
-#The sorted() function sorts the elements of a given iterable in a specific order (either ascending or descending) 
-#and returns the sorted iterable as a list.
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        ans=[]
+        for i in nums:
+            ans.append(i*i)
+        return sorted(ans)
+
