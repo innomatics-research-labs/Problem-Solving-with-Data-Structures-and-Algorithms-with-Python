137994e3d745942812c8dc77688992bae9efbd3f
pavithra <125376819+pavithragithu@users.noreply.github.com>
2023-10-31 03:15:09 -0700

    Update D19P2_LeetCode_771.py

--- Python Solutions/D19P2_LeetCode_771.py
+++ Python Solutions/D19P2_LeetCode_771.py
@@ -1,1 +1,8 @@
-# Python3 code coming soon
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        # for i in stones:
+        count=0
+        for x in stones:
+            if x in jewels:
+                count+=1
+        return count
