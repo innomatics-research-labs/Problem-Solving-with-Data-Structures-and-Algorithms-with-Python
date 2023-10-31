2c99646292d80ac7e9f57409b9e9af57bda3530a
jeromepalayoor <me@jp-ch.gq>
2023-10-31 16:16:41 +0800

    D15P2 solution added

--- Python Solutions/D15P2_LeetCode_973.py
+++ Python Solutions/D15P2_LeetCode_973.py
@@ -1,1 +1,10 @@
 # Python3 code coming soon
+
+class Solution(object):
+    def kClosest(self, points, k):
+        """
+        :type points: List[List[int]]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        return sorted(points, key=lambda x: x[0] ** 2 + x[1] ** 2)[:k]
