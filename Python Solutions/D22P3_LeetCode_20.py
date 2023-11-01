336df173c5ca22dbf166a38964ff469598217c80
Daliparthy Sumukha Kavya <122427131+SumukhaKavya@users.noreply.github.com>
2023-11-01 19:56:23 +0530

    Update D22P3_LeetCode_20.py

--- Python Solutions/D22P3_LeetCode_20.py
+++ Python Solutions/D22P3_LeetCode_20.py
@@ -1,1 +1,12 @@
-# Python3 code coming soon
+class Solution:
+    def isValid(self, s: str) -> bool:
+        mapping = {')':'(',']':'[','}':'{'}
+        stack = []
+        for ch in s:
+            if ch in mapping:
+                top_el = stack.pop() if stack else '#'
+                if top_el != mapping[ch]:
+                    return False
+            else:
+                stack.append(ch)
+        return not stack
