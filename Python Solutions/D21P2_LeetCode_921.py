8adde9f7c18fc68cfc4bfe80f604e428da90e592
Daliparthy Sumukha Kavya <122427131+SumukhaKavya@users.noreply.github.com>
2023-11-01 19:21:23 +0530

    Update D21P2_LeetCode_921.py

--- Python Solutions/D21P2_LeetCode_921.py
+++ Python Solutions/D21P2_LeetCode_921.py
@@ -1,1 +1,13 @@
-# Python3 code coming soon
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        stack = []
+        count = 0
+        for ch in s:
+            if ch == '(':
+                stack.append(ch)
+            else:
+                if stack and stack[-1] == '(':
+                    stack.pop()
+                else:
+                    count += 1
+        return count+len(stack)
