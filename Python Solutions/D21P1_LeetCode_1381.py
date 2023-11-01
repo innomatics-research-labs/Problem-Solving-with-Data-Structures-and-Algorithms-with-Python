e5560dc33a7c107364cbcfcab71c227cc2db7269
Daliparthy Sumukha Kavya <122427131+SumukhaKavya@users.noreply.github.com>
2023-11-01 19:10:58 +0530

    Update D21P1_LeetCode_1381.py

--- Python Solutions/D21P1_LeetCode_1381.py
+++ Python Solutions/D21P1_LeetCode_1381.py
@@ -1,1 +1,18 @@
-# Python3 code coming soon
+class CustomStack:
+    def __init__(self, maxSize: int):
+        self.maxSize = maxSize
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        if len(self.stack) < self.maxSize:
+            self.stack.append(x)
+
+    def pop(self) -> int:
+        if not self.stack:
+            return -1
+        else:
+            return self.stack.pop()
+
+    def increment(self, k: int, val: int) -> None:
+        for i in range(min(k,len(self.stack))):
+            self.stack[i] += val
